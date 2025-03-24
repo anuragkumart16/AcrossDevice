2c52b8dfea4d4e362a2086372b83ade7f256a4e2
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2025-03-24 15:01:25 +0530

    added changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,12 @@
 export default function Home() {
+    const promise = fetch('http://127.0.0.1:8000/checkusername',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({username: 'anurag', password: 'anurag',email: 'anurag'})
+    })
+    promise.then((res) => res.json()).then((err) => console.log(err))
     return (
         <div>
             <h1>Home</h1>
