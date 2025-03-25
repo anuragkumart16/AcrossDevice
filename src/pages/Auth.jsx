c85c7518d0b54b327aca4364098d37b608bcfb53
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2025-03-25 13:09:24 +0530

    added changes to auth page

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -4,13 +4,14 @@ import "./css/auth.css";
 import {motion} from "framer-motion"
 export default function Auth() {
     const [isLogin, setIsLogin] = useState(true)
+    // console.log(motion)
     return (
         <section className="auth-section">
             <img src="access.png" className="auth-sideimg" />
             <div className="auth-left-div">
 
                 <div className="auth-user-pref">
-                    <h1 style={{ fontSize: "2rem" }}>You want to ...</h1>
+                    <h1 style={{ fontSize: "2rem",fontWeight: "300" }}>You want to ...</h1>
                     <div className="auth-btn-holder">
                         <Btn
                             type = {isLogin ? "black" : "primary"}
@@ -29,7 +30,32 @@ export default function Auth() {
                     </div>
                 </div>
 
-                
+                {isLogin &&(<motion.div
+                        className="auth-login"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 0.5 }}
+                    >
+                        <h1 style={{ fontSize: "1.8rem", fontWeight: "300" }}>Login to an account</h1>
+                        <div className="auth-input-grp">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" placeholder="Username" className="auth-input" name="username"/>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" placeholder="Password" className="auth-input" name="password"/>
+                        <Btn type="secondary-thin" text="Login" onclick={() => {}} style={{margin:'10px 0px'}}/>
+                        </div>
+                    </motion.div>)}
+
+
+                {
+                    !isLogin && (<motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 0.5 }}
+                    >
+
+                    </motion.div>)
+                }
             </div>
         </section>
     );
